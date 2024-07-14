2e46f88fb9925c0a5a70db84226c14f0e30b40b9
UditNayak <uditnayak027@gmail.com>
2024-07-14 21:08:04 +0530

    Implemented Detail Component

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,8 +1,19 @@
 import "./detail.css";
+import avatar from "../../images/avatar.png";
 
 const Detail = () => {
   return (
-    <div className="detail"> Detail </div>
+    <div className="detail">
+      <div className="user">
+        <img src={avatar} alt="Avatar"  />
+        <h2>Udit</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div className="info">
+        <button>Block User</button>
+        <button>Log out</button>
+      </div>
+    </div>
   )
 }
 
