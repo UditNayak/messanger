42c6c5eaf87a73a886d96a897d1e735398a3ff69
UditNayak <uditnayak027@gmail.com>
2024-07-14 20:24:03 +0530

    Chat Component: Implemented Top & Bottom Section.

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,30 @@
 import "./chat.css";
+import avatar from "../../images/avatar.png";
+import info from "../../images/info.png";
+
 
 const Chat = () => {
   return (
-    <div className="chat">Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src={avatar} alt="Avatar" />
+          <div className="texts">
+            <span>Karan</span>
+            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src={info} alt="Info" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <input type="text" placeholder="Type a message...." />
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
