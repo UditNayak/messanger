fd3c4c2700d79960cc1c6dd6aa9e5f49c2104aaa
UditNayak <uditnayak027@gmail.com>
2024-07-15 02:47:21 +0530

    Auto-Scroll Feature for Chat Messages using useRef Hook

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,19 @@
 import "./chat.css";
+import { useRef, useEffect } from "react";
 import avatar from "../../images/avatar.png";
 import info from "../../images/info.png";
 
-
 const Chat = () => {
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+  , []);
+
+
+
   return (
     <div className="chat">
       <div className="top">
@@ -11,7 +21,10 @@ const Chat = () => {
           <img src={avatar} alt="Avatar" />
           <div className="texts">
             <span>Karan</span>
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
           </div>
         </div>
         <div className="icons">
@@ -21,49 +34,71 @@ const Chat = () => {
       <div className="center">
         <div className="message own">
           <div className="texts">
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message">
           <img src={avatar} alt="Avatar" />
           <div className="texts">
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message own">
           <div className="texts">
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message">
           <img src={avatar} alt="Avatar" />
           <div className="texts">
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message own">
           <div className="texts">
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message">
           <img src={avatar} alt="Avatar" />
           <div className="texts">
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
         <div className="message own">
           <div className="texts">
-            <p>Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan</p>
+            <p>
+              Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem ipsan Lorem
+              ipsan Lorem ipsan
+            </p>
             <span>1 min ago</span>
           </div>
         </div>
+        <div ref={endRef}></div>
       </div>
       <div className="bottom">
         <input type="text" placeholder="Type a message...." />
